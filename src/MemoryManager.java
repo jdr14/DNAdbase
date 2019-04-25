a78713d9a63fa8fe263ef398df83e5a8ee537e12
jovany <jovanyc4@vt.edu>
2019-04-25 15:41:45 -0400

    begin working with mm.java

--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -1,12 +1,23 @@
 import java.io.*;
 
+/**
+ * 
+ * @author jovany cabrera jovanyc4
+ * @author joey destin rodgers jdr14
+ * @version 1.3.4
+ *
+ */
 public class MemoryManager 
 {
 	/**
 	 * Hash file object
 	 */
 	private RandomAccessFile hashFile;
-
+    
+	/**
+	 * Default constructor
+	 * @param hashFileName of type string
+	 */
 	public MemoryManager(String hashFileName) 
 	{
 		try
@@ -19,6 +30,34 @@ public class MemoryManager
 			System.err.println("Error with creating the hash file " 
 		        + e.getMessage());
 		}
+	}
+	
+	/**
+	 * 
+	 * @param sequence of type string
+	 * @throws IOException
+	 */
+	public void fileInsert(String sequence) throws IOException
+	{
+		// get current hash file location
+		long currentPointer = hashFile.getFilePointer();
+		
+		// write string to hash file as bytes
+		hashFile.write(sequence.getBytes());
+	}
+	
+	/**
+	 * 
+	 * @param seqPosition
+	 * @throws IOException
+	 */
+	public void fileRemove(long seqPosition) throws IOException
+	{
+		// move file pointer to position of sequence to be deleted
+		hashFile.seek(seqPosition);
+		
+		// delete sequence
+		
 	}
     
 }
