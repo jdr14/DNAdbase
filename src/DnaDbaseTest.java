05a38c2b4fa0ac7633f44d0847568896fc57521d
Joey Rodgers <jdr14@vt.edu>
2019-05-03 13:58:19 -0400

    Added testing file with complete testing for the pair class.

--- src/DnaDbaseTest.java
+++ src/DnaDbaseTest.java
@@ -1,8 +1,35 @@
+import java.util.*;
 
-public class DnaDbaseTest {
+import student.TestCase;
 
-	public DnaDbaseTest() {
+public class DnaDbaseTest extends TestCase
+{
+
+	public DnaDbaseTest() 
+	{
 		// TODO Auto-generated constructor stub
 	}
 
+    /**
+     * test function for pair
+     */
+    public void testPair()
+    {
+        Pair<Integer, String> p = new Pair<Integer, String>();
+        Pair<Integer, String> p2 = new Pair<Integer, String>(2, "test");
+        
+        p.setKey(1);
+        p.setValue("testing");
+        
+        int i = p.getKey();
+        String j = p.getValue();
+        
+        assertEquals(i, 1);
+        assertEquals(j, "testing");
+        
+        i = p2.getKey();
+        j = p2.getValue();
+        assertEquals(i, 2);
+        assertEquals(j, "test");
+    }
 }
